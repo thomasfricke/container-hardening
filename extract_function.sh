b6074ce69e3018bfada39a44b86c4be44f39f212
Thomas Fricke <github@thomasfricke.de>
2021-12-08 15:39:11 +0100

    initial version

---
+++ extract_function.sh
@@ -0,0 +1,99 @@
+
+usage(){
+   echo 'extract -d <dynamically linked> -f <files and dirs> -r <files to remove> -u user <files to chown to user>' 
+}
+
+
+next_section(){
+  [ $# -gt 0 ] && [ `echo $1 | head -c 1` != '-' ] && return 0
+  return 1
+}
+
+ldd_filter(){
+  sed 's+\t*++' |\
+  sed 's+.*=>\ ++' |\
+  sed 's+\ .*$++'
+}
+
+link_filter(){
+  for f in $(find "$1")
+  do
+    echo $f
+    if [ -L $f ] 
+      then
+        LINK=$(readlink $f) 
+        if [ `echo $LINK | head -c 1` = '/' ]
+        then
+          echo $LINK
+        else
+          echo $(dirname $f)/$(readlink $f)
+        fi
+     fi
+   done
+}
+
+
+extract(){
+
+  while [ $# -ne 0 ]
+  do	  
+    case $1 in
+    -x) # enable debugging
+
+      set -x 
+      shift
+      ;;
+
+    -d)     # dynamically linked executables
+    
+      shift
+      if next_section $*
+      then
+        for f in $(ldd "$1" | ldd_filter) $1
+        do
+          link_filter $f
+        done
+        shift
+      fi
+      ;;
+
+    -f) # files and links
+    
+      shift
+      while next_section $*
+      do
+        link_filter $1
+        shift
+      done
+      ;;
+
+    -r) # files to remove
+      shift
+      while next_section $*
+      do
+        rm $1
+        shift
+      done
+      ;;
+
+    -u)  # change owner and grand access    
+    
+      OWNER=$1
+      shift
+      while next_section $*
+      do
+        chown $OWNER $1
+        chmod -R +rw $1
+        shift
+      done
+      ;;
+    
+    *) # error, show usage 
+    
+      usage
+      exit 1
+      ;;
+    esac
+  done  | uniq | sed 's+^/++'
+}
+
